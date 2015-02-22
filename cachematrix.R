06954872d449a7a8fe9564721246de00eaa401bb
Tiago Caruso <tiagobcaruso@gmail.com>
2015-02-22 11:10:40 -0800

    Improved the description of the function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,9 @@
 
-## Creates cacheable matrix for inputting to
-## cacheSolve() function which sets and gets
-## the cached values
+## This file creates cacheable matrix and the cacheSolve()
 
 makeCacheMatrix <- function(matrix0 = matrix()) {
     
-    # Let's check if we have correct input
+    # Let's check if we have a matrix
     if (!is.matrix(matrix0)) {
         stop("This is not a matrix")
     }
@@ -19,7 +17,7 @@ makeCacheMatrix <- function(matrix0 = matrix()) {
     
     # Functions for getting and setting cached inv. matrix value
     get <- function() matrix0
-    # Inversing the matrix using build in solve() function in R
+    # Inversing the matrix
     set.inverse <- function(solve) inverted.matrix <<- solve
     get.inverse <- function() inverted.matrix
     
@@ -32,13 +30,12 @@ makeCacheMatrix <- function(matrix0 = matrix()) {
 
 cacheSolve <- function(matrix1, ...) {
     inverted.matrix <- matrix1$get.inverse()
-    # Do we have already have this cached matrix?
+    # Do we have already have this matrix in cache?
     if(!is.null(inverted.matrix)) {
         message("Getting cached inverse matrix")
         return(inverted.matrix)
     }
-    # Let's create inverted matrix in case
-    # there's no cached matrix available.
+    # Creatating inverted matrix if needed.
     matrix.to.inverse <- matrix1$get()
     inverted.matrix <- solve(matrix.to.inverse)
     matrix1$set.inverse(inverted.matrix)
